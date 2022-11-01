5d5d94477a883aa20a3a4274b6eb54133df63a9d
lrondon prog <feliperondon123@hotmail.com>
2022-11-01 14:08:37 -0400

    Create components mobile

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "native-base";
+import { Loading } from "../components/Loading";
+
+export function SigIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">
+        SigIn
+      </Text>
+    </Center>
+  );
+}
