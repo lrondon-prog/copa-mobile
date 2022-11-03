67fb5404bdc3124d087d30c371c34bde2e766de0
lrondon prog <feliperondon123@hotmail.com>
2022-11-03 18:12:13 -0400

    Create logo app

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,12 +1,12 @@
 import { Center, Text } from "native-base";
-import { Loading } from "../components/Loading";
+
+import Logo from "../assets/logo.svg";
 
 export function SigIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        SigIn
-      </Text>
+      <Logo width={212} height={40}/>
+
     </Center>
   );
 }
