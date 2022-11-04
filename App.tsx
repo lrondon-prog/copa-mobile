bf249bff9ab1767c6fa9e4c1a9d2972f36d8adfa
lrondon prog <feliperondon123@hotmail.com>
2022-11-04 14:20:34 -0400

    New components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import { SigIn } from './src/screens/Signin';
+import { New } from './src/screens/New';
 import { Loading } from './src/components/Loading';
 import { THEME } from './src/styles/theme';
 
@@ -17,7 +17,7 @@ export default function App() {
             backgroundColor="transparent"
             translucent
       />
-      {fontsLoaded ? <SigIn /> : <Loading />}
+      {fontsLoaded ? <New /> : <Loading />}
           </AuthContextProvider>
     </NativeBaseProvider>
   );
